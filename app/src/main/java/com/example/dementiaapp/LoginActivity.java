0dc534b7c64cc0d2e3d82a6df31d5bfb7bdd81a6
alexlau64 <0133502@kdu-online.com>
2023-02-10 15:49:23 +0800

    login done

--- app/src/main/java/com/example/dementiaapp/LoginActivity.java
+++ app/src/main/java/com/example/dementiaapp/LoginActivity.java
@@ -1,9 +1,17 @@
 package com.example.dementiaapp;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.dementiaapp.ui.dashboard.DashboardFragment;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -12,5 +20,25 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.login);
+        Login();
+    }
+
+    void Login(){
+        EditText username = (EditText) findViewById(R.id.username_editText);
+        EditText password = (EditText) findViewById(R.id.password_editText);
+        Button btnlogin = (Button) findViewById(R.id.loginButton);
+
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
+                    Toast.makeText(LoginActivity.this, "Login Successfully", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(LoginActivity.this, "Username or Password is incorrect!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
