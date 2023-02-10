a642bba537a7b96207379590cf4ed6272ff1ccca
Eddi3Ho <eddieho2001@gmail.com>
2023-02-10 23:57:49 +0800

    Testing for firestore database add function

--- app/src/main/java/com/example/dementiaapp/LoginActivity.java
+++ app/src/main/java/com/example/dementiaapp/LoginActivity.java
@@ -1,9 +1,22 @@
 package com.example.dementiaapp;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -12,5 +25,38 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.login);
+        Button btntest = (Button) findViewById(R.id.button);
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        btntest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Map<String, Object> user = new HashMap<>();
+                user.put("first", "Ada");
+                user.put("last", "Lovelace");
+                user.put("born", 1815);
+
+                db.collection("users")
+                        .add(user)
+                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+                                Toast.makeText(getApplicationContext(),"DocumentSnapshot added with ID: " + documentReference.getId(),Toast.LENGTH_SHORT).show();
+//                                Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(getApplicationContext(), "Fail",Toast.LENGTH_SHORT).show();
+//                                Log.w(TAG, "Error adding document", e);
+                            }
+                        });
+            }
+        });
+
+
+
+
     }
 }
