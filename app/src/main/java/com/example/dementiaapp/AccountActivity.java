5ffeb0db633f11bfdc6849e80365f055cb33c277
Eddi3Ho <eddieho2001@gmail.com>
2023-02-18 11:12:29 +0800

    update share function message

--- app/src/main/java/com/example/dementiaapp/AccountActivity.java
+++ app/src/main/java/com/example/dementiaapp/AccountActivity.java
@@ -309,6 +309,7 @@ public class AccountActivity extends AppCompatActivity {
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
         dialog.setCancelable(false);
         dialog.setContentView(R.layout.email_friend_dialog);
+        User user = User.getInstance();
 
         Button confirmButton = (Button) dialog.findViewById(R.id.confirmEmailButton);
         confirmButton.setOnClickListener(new View.OnClickListener() {
@@ -317,8 +318,8 @@ public class AccountActivity extends AppCompatActivity {
                 dialog.dismiss();
                 Intent myIntent = new Intent(Intent.ACTION_SEND);
                 myIntent.setType("text/plain");
-                String body = "Your body here";
-                String sub = "Your Subject";
+                String body = "Your friend " + user.getFull_name() + " is inviting you to learn more about dementia. Download the app now (link)";
+                String sub = "Invitation";
                 myIntent.putExtra(Intent.EXTRA_SUBJECT,sub);
                 myIntent.putExtra(Intent.EXTRA_TEXT,body);
                 startActivity(Intent.createChooser(myIntent, "Share Using"));
